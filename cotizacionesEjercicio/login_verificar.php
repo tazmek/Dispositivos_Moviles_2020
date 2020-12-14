e5e906b49048aaca9ce4b310f15dac4febdfaea2
tazmek <71020784+tazmek@users.noreply.github.com>
2020-12-13 23:17:02 -0600

    Add files via upload

--- cotizacionesEjercicio/login_verificar.php
+++ cotizacionesEjercicio/login_verificar.php
@@ -1,31 +1,30 @@
 <?php
 include 'inc/conexion.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $usuario_nombre = $_POST['usuario_nombre'];
-    $usuario_password = $_POST['usuario_password'];
-    
-    $ins=$con->prepare("SELECT FROM usuarios WHERE usuarios nombre= AND usuarios_password VALUES(?,?,?,?,?,?)");
-    $ins->bind_param("isssss",$id,$nombre_del_proveedor_post,$direccion_del_proveedor_post,$telefono_1_post,$telefono_2_post,$correo_proveedor_post);
-    if($ins->execute()){
-        header("Location: alerta.php?tipo=exito&operacion=Proveedor Guardado&destino=proveedor_registrar.php");
-    }
-    else{
-        header("Location: alerta.php?tipo=fracaso&operacion=Proveedor No Guardado&destino=proveedor_registrar.php");
-    }
-    $ins->close();
-    $con->close();
-    /*$nombre_del_proveedor_post = strtoupper($_POST['nombre_del_proveedor']);
-    $direccion_del_proveedor_post = strtoupper($_POST['direccion_del_proveedor']);
-    $telefono_1_post = strtoupper($_POST['telefono_1']);
-    $telefono_2_post = strtoupper($_POST['telefono_2']);
-    $correo_proveedor_post = strtoupper($_POST['correo_proveedor']);
-    
-    $ins=$con->query("INSERT INTO proveedor VALUES(NULL,'$nombre_del_proveedor_post','$direccion_del_proveedor_post','$telefono_1_post','$telefono_2_post','$correo_proveedor_post')");
-    if($ins){
-        echo "Proveedor Registrado";
-    }
-    else{
-        echo "NO SE HA EFECTUADO EL REGISTRO";
-    }
-    $con->close();*/
+$user = $_POST['usuario_nombre'];
+$password = $_POST['usuario_password'];
+$sel = $con->prepare("SELECT *from usuarios where usuarios_nombre=? and usuarios_pass=?");
+$parametro = 50;
+$sel->bind_param('ss', $user, $password);
+$sel->execute();
+$res = $sel->get_result();
+$row = mysqli_num_rows($res);
+if ($row != 0) {
+while ($f = $res->fetch_assoc()) {
+if ($f['usuarios_tipo'] == "empleado") {
+session_start();
+$_SESSION['usuario_tipo'] = "empleado";
+$_SESSION['usuario_nombre'] = $f['usuarios_nombre'];
+header("Location: operaciones_empleado.php");
+} else {
+session_start();
+$_SESSION['usuario_tipo'] = "director";
+$_SESSION['usuario_nombre'] = $f['usuarios_nombre'];
+header("Location: inc/incluye_menu.php");
 }
+}
+} else {
+echo "NOMBRE O PASSWORD INCORRECTO";
+}
+}
+?>
